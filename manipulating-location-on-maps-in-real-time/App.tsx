2e9e2337fcb71db3cf6385fe92c6688f21522ad5
Desenvolvimento <desenvolvimento@zaal.com.br>
2024-04-22 08:27:54 -0300

    feat: center & perspective from maps

--- manipulating-location-on-maps-in-real-time/App.tsx
+++ manipulating-location-on-maps-in-real-time/App.tsx
@@ -2,13 +2,17 @@ import { StyleSheet, View } from 'react-native';
 import { 
   requestForegroundPermissionsAsync, 
   getCurrentPositionAsync,
-  LocationObject
+  LocationObject,
+  watchPositionAsync,
+  LocationAccuracy
 } from 'expo-location';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import MapView from 'react-native-maps';
 
 export default function App() {
   const [location, setLocation] = useState<LocationObject | null>(null);
+
+  const mapRef = useRef<MapView>(null);
 
   async function requestLocationPermissions() {
     const { granted } = await requestForegroundPermissionsAsync();
@@ -24,17 +28,34 @@ export default function App() {
     requestLocationPermissions();
   }, []);
 
+  useEffect(() => {
+    watchPositionAsync({
+      accuracy: LocationAccuracy.Highest,
+      timeInterval: 1000,
+      distanceInterval: 1,
+    }, (response) => {
+      setLocation(response);
+
+      mapRef.current?.animateCamera({
+        pitch: 70,
+        center: response.coords,
+      })
+    })
+  }, [])
+
   return (
     <View style={styles.container}>
-      <MapView 
-        style={styles.map} 
-        initialRegion={{ 
-          latitude: location.coords.latitude, 
-          longitude: location.coords.longitude,
-          latitudeDelta: 0.005,
-          longitudeDelta: 0.005,
-        }} 
-      />
+      {location ? 
+        <MapView 
+          style={styles.map} 
+          ref={mapRef}
+          initialRegion={{ 
+            latitude: location.coords.latitude, 
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005,
+          }} 
+        /> : null}
     </View>
   );
 }
